4e4b4e06b39f29f4f75c9cd53322782eedd269e5
eeyas29 <88485973+eeyas29@users.noreply.github.com>
2022-02-10 14:24:29 +0000

    GPI for ATTN pin added

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -274,6 +274,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
+  /*Configure GPIO pin : PE11 */
+  GPIO_InitStruct.Pin = GPIO_PIN_11;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
