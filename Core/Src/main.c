717cd7c7099e29e4c21a80606699fbdc7d508ae0
Omar Abouelleil <36559888+oabouelleil@users.noreply.github.com>
2022-04-25 15:21:05 +0100

    Formatting

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -457,7 +457,8 @@ void Error_Handler(void)
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  while (1) {
+  while (1)
+  {
   }
   /* USER CODE END Error_Handler_Debug */
 }
